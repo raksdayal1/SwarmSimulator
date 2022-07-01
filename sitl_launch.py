0e89517a9f8fd62b40086c6b5c704031a1690f84
Ethan O'Connor <ethan.oconnor@okstate.edu>
2022-07-01 11:05:42 -0500

    General optimization including: extracted rover data, added tests ensuring init.json input matches up, improved append process to AP_Commands, fixed and improved exiting procedure at the end

--- sitl_launch.py
+++ sitl_launch.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 import time
 import json
 import navpy
@@ -37,10 +38,30 @@ fw_X_Pos = fwData['Xstart']
 fw_Y_Pos = fwData['Ystart']
 fw_Z_Pos = fwData['Zstart']
 
+# Extract rover data (model spawning still needed)
+rover_instances = roverData['InstanceNo']
+rover_X_Pos = roverData['Xstart']
+rover_Y_Pos = roverData['Ystart']
+rover_Z_Pos = roverData['Zstart']
+
 # Get the home location
 lat_ref = data["Home"]["latitude"]
 lon_ref = data["Home"]["longitude"]
 alt_ref = data["Home"]["altitude"]
+
+# Get totals for testing
+all_instances = quad_instances + fw_instances + rover_instances
+all_Pos_values = quad_X_Pos + quad_Y_Pos + quad_Z_Pos + fw_X_Pos + fw_Y_Pos + fw_Z_Pos + rover_X_Pos + rover_Y_Pos + \
+                 rover_Z_Pos
+
+# Series of tests to ensure init.json input matches up
+if total_no_of_vehicles != len(all_instances):
+    sys.exit('\x1b[0;31;40m' + 'Error: Each Instance must have exactly one corresponding InstanceNo' + '\x1b[0m')
+elif len(all_instances) != len(set(all_instances)):
+    sys.exit('\x1b[0;31;40m' + 'Error: All vehicle Instance Numbers must be unique' + '\x1b[0m')
+elif len(all_instances) * 3 != len(all_Pos_values):
+    sys.exit('\x1b[0;31;40m' + 'Error: Each instance must have exactly one corresponding Xstart, Ystart, and Zstart' +
+             '\x1b[0m')
 
 Sdf = CreateRunwayWorld()
 Counter = 0
@@ -56,12 +77,11 @@ for i in quad_instances:
     ned = [quad_X_Pos[Counter], quad_Y_Pos[Counter], quad_Z_Pos[Counter]]
     lla_out = navpy.ned2lla(ned, lat_ref, lon_ref, alt_ref, latlon_unit='deg', alt_unit='m', model='wgs84')
 
-    ap_cmd = ["sim_vehicle.py", "-vArduCopter", "-fgazebo-iris", f"-I{i}",
-              "--auto-sysid", f"--custom-location={lla_out[0]},{lla_out[1]},{lla_out[2]},0"]
+    Ap_Commands.append(["sim_vehicle.py", "-vArduCopter", "-fgazebo-iris", f"-I{i}",
+              "--auto-sysid", f"--custom-location={lla_out[0]},{lla_out[1]},{lla_out[2]},0"])
 
     udp_port = 14550 + 10 * i
     mavproxy_exe.append(f"--master=udp:127.0.0.1:{udp_port}")
-    Ap_Commands.append(ap_cmd)
     Counter += 1
 
 Counter = 0
@@ -72,13 +92,11 @@ for j in fw_instances:
     ned = [fw_X_Pos[Counter], fw_Y_Pos[Counter], fw_Z_Pos[Counter]]
     lla_out = navpy.ned2lla(ned, lat_ref, lon_ref, alt_ref, latlon_unit='deg', alt_unit='m', model='wgs84')
 
-    ap_cmd = ["sim_vehicle.py", "-v ArduPlane", "-f gazebo-zephyr", f"-I{j}",
-              "--auto-sysid ", f"--custom-location=({lla_out[0]},{lla_out[1]},{lla_out[2]},0)"]
+    Ap_Commands.append(["sim_vehicle.py", "-v ArduPlane", "-f gazebo-zephyr", f"-I{j}",
+              "--auto-sysid ", f"--custom-location=({lla_out[0]},{lla_out[1]},{lla_out[2]},0)"])
 
     udp_port = 14550 + 10 * j
     mavproxy_exe.append(f"--master=udp:127.0.0.1:{udp_port}")
-
-    Ap_Commands.append(ap_cmd)
     Counter += 1
 
 Sdf += GetFooter()
@@ -96,7 +114,7 @@ with open("../worlds/Created_Universe.world", "w") as file:
 print("Created a new simulation world")
 
 time.sleep(1)
-Ap_Commands.append(["gazebo", "--verbose", "../worlds/Created_Universe.world"])
+Ap_Commands.insert(0, ["gazebo", "--verbose", "../worlds/Created_Universe.world"])
 Ap_Commands.append(mavproxy_exe)
 
 filePathapm = '/mnt/d/Projects/CounterUAS_COE/ros_ws/src/cowboy_swarm/launch/apm2_multitest.launch'
@@ -135,14 +153,12 @@ print(Ap_Commands)
 procs = []
 for cmd in Ap_Commands:
     procs.append(Popen(cmd, stdout=DEVNULL, stderr=DEVNULL))
-    #procs.append(Popen(cmd))
     time.sleep(1)
 
 procs.append(Popen(["roslaunch", "cowboy_swarm", "apm2_multitest.launch"]))
-for p in procs:
-    p.wait()
 
-time.sleep(2)
+procs[0].wait()  # Only waiting on gazebo window
+
 if os.path.exists(filePath):
     os.remove(filePath)
     print("Deleted simulation file")
@@ -152,4 +168,10 @@ if os.path.exists(filePathapm):
     os.remove(filePathapm)
     print("Deleted Roslaunch apm simulation file")
 
+exit_cmds = ['fuser -k 11311/tcp', 'fuser -k 3456/tcp', 'pkill mavproxy.py', 'pkill mavproxy.py']
+for c in exit_cmds:
+    kill = Popen(f"{c}", shell=True)
+    kill.wait()
+    print(kill.returncode)
 
+sys.exit('\x1b[1;33m' + "Now Exiting Simulation" + '\x1b[0m')
